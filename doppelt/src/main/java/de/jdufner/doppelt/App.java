cfd81ff97337f33395db2ae9ffd4f6ae18ff3fef
Juergen Dufner <juergen@jdufner.de>
2015-06-08 22:46:34 +0200

    Initialer Commit

---
+++ doppelt/src/main/java/de/jdufner/doppelt/App.java
@@ -0,0 +1,13 @@
+package de.jdufner.doppelt;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
