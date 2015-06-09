26510d49a9798b0bd03dc37c66b505f0b1352122
Juergen Dufner <juergen@jdufner.de>
2015-06-10 01:49:25 +0200

    läuft, aber langsam

--- doppelt/src/main/java/de/jdufner/doppelt/DoppeltBuilder.java
+++ doppelt/src/main/java/de/jdufner/doppelt/DoppeltBuilder.java
@@ -1,5 +1,69 @@
 package de.jdufner.doppelt;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 public class DoppeltBuilder {
+
+	private int size = 0;
+	private int anzahlElements = 0;
+
+	private Set<SortedSet<Integer>> tupels = new HashSet<SortedSet<Integer>>();
+
+	public Set<SortedSet<Integer>> build(final int size) {
+		this.size = size;
+		this.anzahlElements = size + (size - 1) * (size - 1);
+		buildTupel(1, new TreeSet<Integer>());
+		return tupels;
+	}
+
+	public Set<SortedSet<Integer>> reduce(final int size) {
+		final Set<SortedSet<Integer>> allTupels = build(size);
+		final Set<SortedSet<Integer>> oneByOneTupels = new HashSet<SortedSet<Integer>>();
+		for (final SortedSet<Integer> tupel : allTupels) {
+			if (!hasMoreThenOneLink(tupel, oneByOneTupels)) {
+//				System.out.println(tupel);
+				oneByOneTupels.add(tupel);
+			}
+		}
+		return oneByOneTupels;
+	}
+
+	private boolean hasMoreThenOneLink(final SortedSet<Integer> tupel,
+			final Set<SortedSet<Integer>> oneByOneTupels) {
+		for (final SortedSet<Integer> oneByOneTupel : oneByOneTupels) {
+			final Collection<Integer> links = new HashSet<Integer>();
+			for (final Integer element : tupel) {
+				if (oneByOneTupel.contains(element)) {
+					links.add(element);
+				}
+			}
+			if (links.size() > 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private void buildTupel(final int stacksize,
+			final SortedSet<Integer> sortedSet) {
+		for (int i = 1; i <= anzahlElements; i++) {
+			final SortedSet<Integer> sortedSet2 = new TreeSet<Integer>(
+					sortedSet);
+			if (sortedSet2.add(i)) {
+				if (stacksize < size) {
+					buildTupel(stacksize + 1, sortedSet2);
+				} else {
+//					System.out.println(sortedSet2);
+					if (sortedSet2.size() == size) {
+						tupels.add(sortedSet2);
+					}
+				}
+			}
+		}
+	}
 
 }
--- doppelt/src/test/java/de/jdufner/doppelt/DoppeltBuilderTest.java
+++ doppelt/src/test/java/de/jdufner/doppelt/DoppeltBuilderTest.java
@@ -1,5 +1,12 @@
 package de.jdufner.doppelt;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
 import org.junit.Test;
 
 
@@ -7,7 +14,49 @@ public class DoppeltBuilderTest {
 
 	@Test
 	public void whenBuild2Expect3Tupel() {
-		
+		final DoppeltBuilder builder = new DoppeltBuilder();
+		Set<SortedSet<Integer>> tupels = buildResultTupels();
+		Set<SortedSet<Integer>> listOfTupel = builder.build(2);
+		assertEquals(tupels, listOfTupel);
+	}
+
+	private Set<SortedSet<Integer>> buildResultTupels() {
+		SortedSet<Integer> tupel1 = new TreeSet<Integer>();
+		tupel1.add(1);
+		tupel1.add(2);
+		SortedSet<Integer> tupel2 = new TreeSet<Integer>();
+		tupel2.add(1);
+		tupel2.add(3);
+		SortedSet<Integer> tupel3 = new TreeSet<Integer>();
+		tupel3.add(2);
+		tupel3.add(3);
+		Set<SortedSet<Integer>> tupels = new HashSet<SortedSet<Integer>>();
+		tupels.add(tupel1);
+		tupels.add(tupel2);
+		tupels.add(tupel3);
+		return tupels;
+	}
+	
+	
+	@Test
+	public void whenBuild3AndReduceExpectLinkedByOne() {
+		final DoppeltBuilder builder = new DoppeltBuilder();
+		Set<SortedSet<Integer>> listOfTupel = builder.reduce(3);
+		assertEquals(7, listOfTupel.size());
+	}
+	
+	@Test
+	public void build8() {
+		final DoppeltBuilder builder = new DoppeltBuilder();
+		Set<SortedSet<Integer>> listOfTupel = builder.build(8);
+		System.out.println(listOfTupel);
+	}
+	
+	@Test
+	public void reduce5() {
+		final DoppeltBuilder builder = new DoppeltBuilder();
+		Set<SortedSet<Integer>> listOfTupel = builder.reduce(5);
+		System.out.println(listOfTupel);
 	}
 	
 }
