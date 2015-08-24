e17972a146a189ad631b00974fc3a759b4a99350
Juergen Dufner <juergen@jdufner.de>
2015-08-24 16:17:09 +0200

    2 Profile prod und dev angelegt
    * dev = Development ohne Security
    * prod = Production mit Security

--- doppelt/src/main/java/de/jdufner/doppelt/configuration/SecurityProductionConfiguration.java
+++ doppelt/src/main/java/de/jdufner/doppelt/configuration/SecurityProductionConfiguration.java
@@ -2,6 +2,7 @@ package de.jdufner.doppelt.configuration;
 
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,7 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Profile("prod")
+public class SecurityProductionConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   public void configure(final AuthenticationManagerBuilder auth) throws Exception {
@@ -18,7 +20,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(final HttpSecurity http) throws Exception {
-    http.authorizeRequests().antMatchers("/ui/**").permitAll().antMatchers("/api/**").hasRole("USER").and().httpBasic();
+    // @formatter:off
+    http
+      .authorizeRequests().antMatchers("/ui/**").permitAll()
+      .and()
+      .authorizeRequests().antMatchers("/api/**").hasRole("USER")
+      .and()
+      .httpBasic()
+    ;
+    // @formatter:on
   }
 
 }
