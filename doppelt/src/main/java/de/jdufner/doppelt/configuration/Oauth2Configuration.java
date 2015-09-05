2d6cb42e25da3d35f77123c4d893cf52a982fa59
Juergen Dufner <juergen@jdufner.de>
2015-09-05 21:47:41 +0200

    Zwei unnötige Clients entfernt

--- doppelt/src/main/java/de/jdufner/doppelt/configuration/Oauth2Configuration.java
+++ doppelt/src/main/java/de/jdufner/doppelt/configuration/Oauth2Configuration.java
@@ -26,7 +26,9 @@ public class Oauth2Configuration {
         .requestMatchers().antMatchers("/api/**")
         .and()
         .authorizeRequests()
-        .anyRequest().access("#oauth2.hasScope('read')");
+        .anyRequest()
+        .access("#oauth2.hasScope('read')")
+      ;
       // @formatter:on
     }
 
@@ -53,26 +55,13 @@ public class Oauth2Configuration {
     public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
       // @formatter:off
       clients.inMemory()
-            .withClient("my-trusted-client")
-                .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
-                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
-                .scopes("read", "write", "trust")
-                .resourceIds("sparklr")
-                .accessTokenValiditySeconds(60)
-        .and()
-            .withClient("my-client-with-registered-redirect")
-                .authorizedGrantTypes("authorization_code")
-                .authorities("ROLE_CLIENT")
-                .scopes("read", "trust")
-                .resourceIds("sparklr")
-                .redirectUris("http://anywhere?key=value")
-        .and()
             .withClient("my-client-with-secret")
                 .authorizedGrantTypes("client_credentials", "password")
                 .authorities("ROLE_CLIENT")
                 .scopes("read")
                 .resourceIds("sparklr")
-                .secret("secret");
+                .secret("secret")
+      ;
     // @formatter:on
     }
 
