8219a084a55a8e0418b6c4037f202e9d2a002b30
Juergen Dufner <juergen@jdufner.de>
2015-09-05 21:21:24 +0200

    OAuth2-Authentifizierung aktiviert
    * Authorization- und Resource-Server in einer Klasse
    * Noch ohne Refesh-Token

--- doppelt/readme.md
+++ doppelt/readme.md
@@ -1,5 +1,28 @@
 # Banner
 made by <http://patorjk.com/software/taag/#p=display&f=Graffiti&t=doppelt>
 
-# REST-Controller
-## curl http://user:4262ad3e-e286-4054-affc-5dba52335155@localhost:8080/api/greeting
+# OAuth2
+
+## Ermittlung des Token
+
+Aufruf
+ 
+    curl -H "Accept: application/json" my-client-with-secret:secret@localhost:8080/oauth/token -d grant_type=client_credentials
+ 
+Rückgabe
+
+    {"access_token":"829d21e3-94f4-4bc7-b846-89b687ceeba1","token_type":"bearer","expires_in":43199,"scope":"read"}
+
+## Greeting-Service mit Token aufrufen
+
+Aufruf
+
+    curl -H "Authorization: Bearer 829d21e3-94f4-4bc7-b846-89b687ceeba1" localhost:8080/api/greeting
+ 
+Rückgabe
+
+    {"id":1,"content":"Hello, World!"}
+
+## Referenzen
+* <https://github.com/dsyer/sparklr-boot>
+* <https://raymondhlee.wordpress.com/2014/12/21/implementing-oauth2-with-spring-security/>
---
+++ doppelt/src/main/java/de/jdufner/doppelt/configuration/Oauth2Configuration.java
@@ -0,0 +1,80 @@
+package de.jdufner.doppelt.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+@Configuration
+public class Oauth2Configuration {
+
+  @Configuration
+  @EnableResourceServer
+  protected static class ResourceServer extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+      // @formatter:off
+      http
+        .requestMatchers().antMatchers("/api/**")
+        .and()
+        .authorizeRequests()
+        .anyRequest().access("#oauth2.hasScope('read')");
+      // @formatter:on
+    }
+
+    @Override
+    public void configure(final ResourceServerSecurityConfigurer resources) throws Exception {
+      resources.resourceId("sparklr");
+    }
+
+  }
+
+  @Configuration
+  @EnableAuthorizationServer
+  protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+      endpoints.authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
+      // @formatter:off
+      clients.inMemory()
+            .withClient("my-trusted-client")
+                .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
+                .authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+                .scopes("read", "write", "trust")
+                .resourceIds("sparklr")
+                .accessTokenValiditySeconds(60)
+        .and()
+            .withClient("my-client-with-registered-redirect")
+                .authorizedGrantTypes("authorization_code")
+                .authorities("ROLE_CLIENT")
+                .scopes("read", "trust")
+                .resourceIds("sparklr")
+                .redirectUris("http://anywhere?key=value")
+        .and()
+            .withClient("my-client-with-secret")
+                .authorizedGrantTypes("client_credentials", "password")
+                .authorities("ROLE_CLIENT")
+                .scopes("read")
+                .resourceIds("sparklr")
+                .secret("secret");
+    // @formatter:on
+    }
+
+  }
+}
